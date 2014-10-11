42c78d04b99a85a3e034e2ce4300a9f6d3a73fa3
Ethan Thomas <ethanthomas33@gmail.com>
2014-10-11 13:34:28 -0400

    Removed Keys

--- app/src/main/java/com/hhs/hfnavigator/core/Application.java
+++ app/src/main/java/com/hhs/hfnavigator/core/Application.java
@@ -12,8 +12,7 @@ public class Application extends android.app.Application {
 
     public void onCreate() {
         super.onCreate();
-        Parse.initialize(this, "C9vun7KPxdDKBsVcuzwxlyRDEC83hekR9O6yeX3e",
-                "OoIEedGp2uIu0yWImnlqDTNunEPLaf0O6jTe4IgR");
+        Parse.initialize(this, "Key 1", "Key 2");
         ParseAnalytics.trackAppOpened(getIntent());
         ParseUser.enableAutomaticUser();
         PushService.setDefaultPushCallback(this, Home.class);
